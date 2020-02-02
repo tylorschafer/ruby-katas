1561b92fae3b6a757b227c9936a0e84da85c8095
Tylor Schafer <tylorschafer@gmail.com>
2020-02-01 23:25:22 -0700

    Add working luhn test functionality

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,4 +1,18 @@
 class Luhn
-  def valid?
+  def self.valid?(cc_string)
+    nums = cc_string.tr(' ', '').split('')
+    if nums.length > 1
+      doubles = nums.reverse.each_with_index.map do |num, index|
+        if num.to_i.to_s == num
+          index.odd? ? num.to_i * 2 : num.to_i
+        else
+          return false
+        end
+      end
+      fixed_nums = doubles.map { |num| num > 9 ? num - 9 : num }
+      fixed_nums.sum % 10 == 0
+    else
+      return false
+    end
   end
 end
