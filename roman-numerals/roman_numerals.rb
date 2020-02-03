658c15ffed73b4a1a5a834a05fe75d1b7412d4e1
Tylor Schafer <tylorschafer@gmail.com>
2020-02-03 16:43:59 -0700

    Add basic numeral finding

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,3 +1,21 @@
-def to_roman
+class Integer
+  def numerals
+    {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    }
+  end
 
+  def to_roman
+    numerals.each do |numeral, value|
+      if value === self
+        return numeral.to_s
+      end
+    end
+  end
 end
--- roman-numerals/roman_numerals_test.rb
+++ roman-numerals/roman_numerals_test.rb
@@ -4,7 +4,6 @@ require_relative 'roman_numerals'
 # Common test data version: 1.2.0 3c78ac4
 class RomanNumeralsTest < Minitest::Test
   def test_1_is_a_single_i
-    # skip
     assert_equal 'I', 1.to_roman
   end
 
