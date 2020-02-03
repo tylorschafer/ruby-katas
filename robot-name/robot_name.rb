0864c7be7c23f385ccaff331050f88119173d6e4
Tylor Schafer <tylorschafer@gmail.com>
2020-02-02 22:23:01 -0700

    Refact @@robots to hash for lookup speed

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Robot
-  @@robots = []
+  @@robots = {}
 
   attr_reader :name, :letters, :numbers
 
@@ -12,12 +12,12 @@ class Robot
   def create_name
     letters = 2.times.map { ('a'..'z').to_a.sample }.join.upcase
     numbers = 3.times.map { (0..9).to_a.sample }.join
-    potential_name = letters + numbers
-    if @@robots.include?(potential_name)
+    name = letters + numbers
+    if @@robots[name]
       create_name
     else
-      @@robots << (letters + numbers)
-      letters + numbers
+      @@robots[name] = 'Robot'
+      name
     end
   end
 
