3690ca657c3a326134b6aabb85b7db7cee55dd26
Tylor Schafer <tylorschafer@gmail.com>
2020-02-03 17:26:14 -0700

    Add working Scrabble functionality

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,18 +1,30 @@
 class Scrabble
 
+  @@letter_values = {
+    A: 1, E: 1, I: 1, O: 1, U: 1,
+    L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2, B: 3, C: 3, M: 3,
+    P: 3, F: 4, H: 4, V: 4, W: 4,
+    Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10
+  }
+
   def initialize(word)
     @word = word
-    @letter_values = {
-      A: 1, E: 1, I: 1, O: 1, U: 1,
-      L: 1, N: 1, R: 1, S: 1, T: 1,
-      D: 2, G: 2, B: 3, C: 3, M: 3,
-      P: 3, F: 4, H: 4, V: 4, W: 4,
-      Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10
-    }
   end
 
   def score
-    if @word.strip.length > 0
+    if @word && @word.strip.length > 0
+      letters = @word.split('')
+      letters.sum { |let| @@letter_values[let.upcase.to_sym] }
+    else
+      0
+    end
+  end
+
+  def self.score(word)
+    if word && word.strip.length > 0
+      letters = word.split('')
+      letters.sum { |let| @@letter_values[let.upcase.to_sym] }
     else
       0
     end
